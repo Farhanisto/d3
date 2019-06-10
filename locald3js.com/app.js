ed0c6d0e8deda2dc622c2778d0f0ae8fb3a6f906
Farhan <farhanisto@gmail.com>
2019-06-10 17:48:03 +0300

    functions in methods

--- locald3js.com/app.js
+++ locald3js.com/app.js
@@ -1,7 +1,13 @@
 var data = [5,3,42,23,23,4]
-var d3 = d3.select('body').selectAll('p').data(data).enter().append('p').text('hello sec 5')
+var d3 = d3.select('body')
+           .selectAll('p')
+           .data(data).enter()
+           .append('p')
+           .text(((d)=> 'this is a paragraph'+d))
+           .style('color', (d)=>{
+              return  d < 15 ? 'orange' : 'grey'
+           })
         //    .append('g')
         //    //.attr('class','className')
         //    .text('This is a test')
         //    .classed('className3', true)
-        //    .style('color','red')
