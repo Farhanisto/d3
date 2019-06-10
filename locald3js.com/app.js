0e00d2ab9e00009edf299ab884ea79cfd58abb7a
Farhan <farhanisto@gmail.com>
2019-06-10 17:38:01 +0300

    data and enter methods

--- locald3js.com/app.js
+++ locald3js.com/app.js
@@ -1,6 +1,7 @@
-var d3 = d3.select('body')
-           .append('g')
-           //.attr('class','className')
-           .text('This is a test')
-           .classed('className3', true)
-           .style('color','red')
+var data = [5,3,42,23,23,4]
+var d3 = d3.select('body').selectAll('p').data(data).enter().append('p').text('hello sec 5')
+        //    .append('g')
+        //    //.attr('class','className')
+        //    .text('This is a test')
+        //    .classed('className3', true)
+        //    .style('color','red')
