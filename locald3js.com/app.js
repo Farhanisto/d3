45e4b9ada7d93213e741cb7956b159e932579378
Farhan <farhanisto@gmail.com>
2019-06-10 18:21:19 +0300

    fetch data from csv

--- locald3js.com/app.js
+++ locald3js.com/app.js
@@ -1,13 +1,19 @@
-var data = [5,3,42,23,23,4]
-var d3 = d3.select('body')
-           .selectAll('p')
-           .data(data).enter()
-           .append('p')
-           .text(((d)=> 'this is a paragraph'+d))
-           .style('color', (d)=>{
-              return  d < 15 ? 'orange' : 'grey'
-           })
-        //    .append('g')
-        //    //.attr('class','className')
-        //    .text('This is a test')
-        //    .classed('className3', true)
+d3.csv('data.csv').then((data)=> {
+    console.log(data)
+    generate(data.columns)
+})
+function generate(data){
+    //var data = [5,3,42,23,23,4]
+    var el = d3.select('body')
+               .selectAll('p')
+               .data(data).enter()
+               .append('p')
+               .text(((d)=> 'this is a paragraph'+d))
+               .style('color', (d)=>{
+                  return  d < 15 ? 'orange' : 'grey'
+               })
+            //    .append('g')
+            //    //.attr('class','className')
+            //    .text('This is a test')
+            //    .classed('className3', true)
+}
