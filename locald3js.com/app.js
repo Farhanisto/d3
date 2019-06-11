c7f3b78091c1f97908935087e01364b91de44626
Farhan <farhanisto@gmail.com>
2019-06-11 10:11:15 +0300

    bar chart

--- locald3js.com/app.js
+++ locald3js.com/app.js
@@ -1,19 +1,29 @@
-d3.json('data.json').then((data)=> {
-    console.log(data)
-    generate(data)
-})
-function generate(data){
+var data =[]
+
+for(var i = 0; i<20; i++){
+    var num = Math.floor(Math.random()*60)
+    data.push(num)
+}
+var svg = d3.select("#barchart")
+            .append("svg")
+            .attr("width", 800)
+            .attr("height", 400)
     //var data = [5,3,42,23,23,4]
-    var el = d3.select('body')
-               .selectAll('p')
+    //var el = d3.select('#barchart')
+               svg.selectAll('rect')
                .data(data).enter()
-               .append('p')
-               .text(((d)=> 'this is a paragraph'+d))
-               .style('color', (d)=>{
-                  return  d < 15 ? 'orange' : 'grey'
-               })
+               .append('rect')
+               .attr('x', (d,i) => i*1000/data.length)
+               .attr('y', 0)
+               .attr('width', 800/data.length - 5)
+               .attr('height', d=> d)
+
+            //    .attr("class","bar")
+            //    .style('height', (d)=>{
+            //       return  d + "px"
+            //    })
             //    .append('g')
             //    //.attr('class','className')
             //    .text('This is a test')
             //    .classed('className3', true)
-}
+
