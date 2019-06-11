7cedb7af4bf91a83b7a7b71cbf65e32a9abfeabf
Farhan <farhanisto@gmail.com>
2019-06-11 13:01:02 +0300

    Reverse chart

--- locald3js.com/app.js
+++ locald3js.com/app.js
@@ -1,5 +1,7 @@
 var data =[]
-
+var barchart_width = 800
+var barchart_height = 400
+var bar_padding =5
 for(var i = 0; i<20; i++){
     var num = Math.floor(Math.random()*60)
     data.push(num)
@@ -13,10 +15,11 @@ var svg = d3.select("#barchart")
                svg.selectAll('rect')
                .data(data).enter()
                .append('rect')
-               .attr('x', (d,i) => i*1000/data.length)
-               .attr('y', 0)
-               .attr('width', 800/data.length - 5)
-               .attr('height', d=> d)
+               .attr('x', (d,i) => i*barchart_width/data.length)
+               .attr('y', d=> barchart_height -d * 5)
+               .attr('width', barchart_width/data.length - bar_padding)
+               .attr('height', d=> d * 5)
+               .attr('fill','grey')
 
             //    .attr("class","bar")
             //    .style('height', (d)=>{
